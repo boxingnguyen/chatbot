bc56d9fd98a226eab4a6e105866393f3c650537c
anhnq <anhnq@tmh-techlab.vn>
2016-09-08 09:02:47 +0000

    testBot

---
+++ app.js
@@ -0,0 +1,78 @@
+// # SimpleServer
+// A simple chat bot server
+var logger = require('morgan');
+var http = require('http');
+var bodyParser = require('body-parser');
+var express = require('express');
+var router = express();
+
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+var server = http.createServer(app);
+var request = require("request");
+
+// test
+app.get('/',function(req, res) {
+  res.send("Home page. Server running okay.");
+});
+
+// Đây là đoạn code để tạo Webhook
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.verify_token'] === '1111') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+});
+
+// Xử lý khi có người nhắn tin cho bot
+
+app.post('/webhook', function(req, res) {
+  var entries = req.body.entry;
+  for (var entry in entries) {
+    var messaging = entry.messaging;
+    for (var message of messaging) {
+      var senderId = message.sender.id;
+      if (message.message) {
+        // If user send text
+        if (message.message.text) {
+          var text = message.message.text;
+          console.log(text); // In tin nhắn người dùng
+          sendMessage(senderId, "Tớ là bot đây: " + text);
+        }
+      }
+    }
+  }
+
+  res.status(200).send("OK");
+});
+
+var token = "EAAP23cqqcdoBAMJtKi4bZAinWmOwc6YipeAH0EPmoP4BcCxmnaicljdhYprSA1ZAq8GDwAZA1ZAKOpwgylhZCa6E8mEL2m3ORqbHQo1ZCZA5uppm8YrFabipN6FMowy6Pu0qZBuwDYcHJS6O2wXrZA3ZBV3dRv3rvFSPDWZCTjuFDRJtgZDZD";
+// Gửi thông tin tới REST API để trả lời
+function sendMessage(senderId, message) {
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: "token",
+    },
+    method: 'POST',
+    json: {
+      recipient: {
+        id: senderId
+      },
+      message: {
+        text: message
+      },
+    }
+  });
+}
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
+
+server.listen(app.get('port'), app.get('ip'), function() {
+  console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
+});
